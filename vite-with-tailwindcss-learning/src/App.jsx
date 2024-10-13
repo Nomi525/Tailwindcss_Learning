baa96624358f94c6d8cdfcdc7ae340d5f2efb1c4
NomanMansuri <nomanmansuri525@gmail.com>
2024-10-13 19:16:49 +0530

    inti the tailwind css project

---
+++ vite-with-tailwindcss-learning/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-8xl font-bold text-pink-400">I am tailwindcss</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
