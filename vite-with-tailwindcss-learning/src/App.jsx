c7d4a27bafee3eb273bd9fc2490cd6d2d4cc53c6
NomanMansuri <nomanmansuri525@gmail.com>
2024-10-14 11:17:40 +0530

    learning tl css

--- vite-with-tailwindcss-learning/src/App.jsx
+++ vite-with-tailwindcss-learning/src/App.jsx
@@ -1,10 +1,87 @@
 function App() {
   return (
-    <div>
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-8xl font-bold text-pink-400">I am tailwindcss</h1>
-      </div>
-    </div>
+    <>
+      {/* Text Font Family , Size */}
+      {/* <div className="flex justify-center gap-3 items-center bg-black h-screen text-white">
+        <div>
+          <h1 className="font-sans text-xl italic">I am font sans</h1>
+        </div>
+
+        <div>
+          <h1 className="font-serif text-9xl font-thin">I am font-serif</h1>
+        </div>
+        <div>
+          <h1 className="font-mono text-4xl font-bold">I am font-mono</h1>
+        </div>
+      </div> */}
+      {/* Text Alignment */}
+      {/* <div>
+        <h1 className="text-left"> I am text left</h1>
+        <h1 className="text-right"> I am text right</h1>
+        <h1 className="text-center"> I am text center</h1>
+
+        <p className="text-justify">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni a hic
+          dolor, dolorem atque voluptatem commodi debitis molestias accusamus
+          ducimus veritatis consequuntur saepe perferendis totam repellendus, at
+          quod est illum? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Quasi voluptates optio dignissimos dolorem eius nemo aliquam
+          dolor quaerat voluptate debitis iure minima velit, fuga soluta, facere
+          eum sed suscipit blanditiis.
+        </p>
+      </div> */}
+
+      {/* Margin Padding Border */}
+
+      {/* <div className="flex justify-evenly">
+        <div className="border-8 border-red-700 gap-2 bg-black flex justify-center items-center text-white w-32 h-32 mx-20">
+          <h1 className="pt-20">Box -1</h1>
+          <h1 className="pb-20">Box -1</h1>
+        </div>
+        <div className="border-t-8 border-b-8 border-t-green-700 border-b-red-700 gap-2 bg-black flex justify-center items-center text-white w-32 h-32  my-10">
+          <h1 className="pt-10">Box -1</h1>
+          <h1 className="pb-10">Box -1</h1>
+        </div>
+      </div> */}
+
+      {/* Width Height Custom Square Bracket */}
+      {/* <div> */}
+      {/* <div className="w-full h-screen bg-gradient-to-l from-red-500 to to-blue-500">
+          <h1 className="pl-[100px]">Hello</h1>
+          <h1 className="pl-[300px]">Hello</h1>
+          <h1 className="pl-[600px]">Hello</h1>
+          <h1 className="pl-[900px]">Hello</h1>
+          <h1 className="pl-[1200px]">Hello</h1>
+        </div> */}
+
+      {/* <div className="flex justify-center mt-10">
+          <button
+            type="button"
+            className="bg-yellow-400 p-3 size-1/6 rounded-full border-1 border-black  hover:bg-orange-200"
+          >
+            Submit
+          </button>
+        </div> */}
+
+      {/* BreakPoint */}
+
+      {/* <div>
+          <div
+            className="w-full h-32 bg-black
+          sm:bg-red-600 md:bg-green-500"
+          ></div>
+        </div>
+      </div> */}
+
+      {/* Grid */}
+
+      {/* <div className="grid grid-cols-6 gap-4">
+        <div className="col-start-1 col-span-4 ">01</div>
+        <div className="col-start-1 col-span-2">02</div>
+        {/* <div className="col-end-7 col-span-2">03</div> */}
+        {/* <div className="col-start-1 col-end-7">04</div> */}
+      {/* </div> */}
+    </>
   );
 }
 
